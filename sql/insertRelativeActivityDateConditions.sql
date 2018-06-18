1ade36a1aa2fa615b16e557fe6a8d3007bd9b337
AJ Fasano <ajfasano@gmail.com>
2018-06-18 14:24:54 -0400

    Activity condition for date in the future or past

---
+++ sql/insertRelativeActivityDateConditions.sql
@@ -0,0 +1,4 @@
+INSERT INTO civirule_condition (name, label, class_name, is_active)
+  VALUES('activity_is_future_date', 'Activity Date in the Future', 'CRM_CivirulesCondition_Activity_ActivityIsFuture', 1);
+INSERT INTO civirule_condition (name, label, class_name, is_active)
+  VALUES('activity_is_past_date', 'Activity Date in the Past', 'CRM_CivirulesCondition_Activity_ActivityIsPast', 1);

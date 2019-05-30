f6e9b3143a26269f065d93425b055fdab04fd25b
Seamus Lee <seamuslee001@gmail.com>
2019-05-31 07:52:15 +1000

    civirules#27 Update Test to take into account proper name of API

--- tests/phpunit/api/v3/CiviRuleTag/GetTest.php
+++ tests/phpunit/api/v3/CiviRuleTag/GetTest.php
@@ -42,7 +42,7 @@ class api_v3_CiviRuleTag_GetTest extends \PHPUnit_Framework_TestCase implements 
    * Note how the function name begins with the word "test".
    */
   public function testApiExample() {
-    $result = civicrm_api3('CiviRuleTag', 'Get', array('magicword' => 'sesame'));
+    $result = civicrm_api3('CiviRuleRuleTag', 'Get', array('magicword' => 'sesame'));
     $this->assertEquals('Twelve', $result['values'][12]['name']);
   }
 
